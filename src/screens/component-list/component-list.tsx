a5a907a9d80aa9a8a59f23528645bcbbe5abba25
vaponte <victor.aponte@azeta.com.py>
2024-06-20 18:18:42 -0400

    Agrega: la vista de listado de componentes

--- src/screens/component-list/component-list.tsx
+++ src/screens/component-list/component-list.tsx
@@ -1,10 +1,31 @@
-import { View, Text } from "react-native";
+import { TouchableOpacity, View, Text, StyleSheet } from "react-native";
 
 
-export const ComponentListScreen = () => {
+export const ComponentListScreen = ({ navigation }) => {
 
 
     return (
-        <View><Text>Lista de componente</Text></View>
+        <TouchableOpacity onPress={() => {
+
+            navigation.navigate("ModalScreen")
+        }}>
+            <View style={estiloModalScreen.container}>
+                <Text style={estiloModalScreen.title}>Componente Modal</Text>
+            </View>
+        </TouchableOpacity>
     )
 }
+
+const estiloModalScreen = StyleSheet.create({
+    container: {
+        backgroundColor: "#33cadd",
+        paddingVertical: 25,
+        marginHorizontal: 20,
+        borderRadius: 20
+    },
+    title: {
+        textAlign: "center",
+        color: "#000",
+        fontSize: 17
+    }
+})
