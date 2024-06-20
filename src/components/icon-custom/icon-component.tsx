38b83a846b7557a35382723d60e95212cd3bccf2
vaponte <victor.aponte@azeta.com.py>
2024-06-20 15:51:20 -0400

    Agrega: componente de iconos customizados

---
+++ src/components/icon-custom/icon-component.tsx
@@ -0,0 +1,24 @@
+import { View } from "react-native";
+import React from "react";
+
+import EntypoIcon from "react-native-vector-icons/Entypo";
+import AntDesign from "react-native-vector-icons/AntDesign";
+import FontAwesomeIcon from "react-native-vector-icons/FontAwesome";
+
+export {
+    EntypoIcon,
+    AntDesign,
+    FontAwesomeIcon
+}
+
+import { IconTypes } from "../../types/icons-types";
+
+
+export const IconComponent = (props: IconTypes) => {
+    const { name, size, color } = props
+    return (
+        <>
+            <EntypoIcon name={name} size={size} color={color} />
+        </>
+    )
+}
