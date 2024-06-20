93a7f1f37f96ec5ad19da022e851a1e7ffd30da2
vaponte <victor.aponte@azeta.com.py>
2024-06-20 11:04:24 -0400

    Agrega: configuracion inicial funcional del proyecto

---
+++ src/screens/home/home-screen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native"
+
+
+export const HomeScreen = () => {
+
+
+    return (
+        <View>
+            <Text>Bienvenido al Home</Text>
+        </View>
+    )
+}
---
+++ src/screens/profile/profile-screen.tsx
@@ -0,0 +1,7 @@
+import { View, Text } from "react-native"
+
+export const ProfileScreen = () => {
+    return (
+        <View><Text>Bienvenido al Home</Text></View>
+    )
+}
