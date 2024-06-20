519ae12bcb19cc41e644e43b9046f44dfd0c592b
vaponte <victor.aponte@azeta.com.py>
2024-06-20 18:05:22 -0400

    Agrega: tipado de modal component

---
+++ src/types/types-projecs.tsx
@@ -0,0 +1,13 @@
+export type IconTypes = {
+    name?: string
+    size?: number
+    color?: string
+}
+
+export interface ModalTypes {
+    title: string;
+    subTitle: string;
+    color?: string;
+    titleColor?: string;
+    subTitleColor?: string;
+}
