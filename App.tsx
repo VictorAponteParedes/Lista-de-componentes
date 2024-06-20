6074fb02c71a2fafeebf6f0335b30e7b361213f8
vaponte <victor.aponte@azeta.com.py>
2024-06-20 18:08:46 -0400

    Ajuste: modificacion de App.tsx

--- App.tsx
+++ App.tsx
@@ -6,39 +6,21 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 //NAVIGATIONS
-import { NavigationTabs } from "./src/navigation/navigation-tab"
+import { NavigationTabs } from "./src/navigation/navigation-tab";
+import { MyNavigationStack } from "./src/navigation/navigations-screen";
 
 
 import { HomeScreen } from './src/screens/home/home-screen';
 import { ProfileScreen } from './src/screens/profile/profile-screen';
 
+
+
 const Stack = createNativeStackNavigator();
-
-
-
-
-
 
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Tabs"
-          component={NavigationTabs}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: 'Welcome' }}
-        />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
-
-
-      </Stack.Navigator>
-    </NavigationContainer>
+    <MyNavigationStack />
   );
 };
 
