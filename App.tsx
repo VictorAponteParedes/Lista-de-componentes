a5caa4b10e18186707901b10b4f8739a36355383
vaponte <victor.aponte@azeta.com.py>
2024-06-20 15:55:59 -0400

    Agrega: modificacion en la vista App.tsx

--- App.tsx
+++ App.tsx
@@ -5,6 +5,9 @@ import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+//NAVIGATIONS
+import { NavigationTabs } from "./src/navigation/navigation-tab"
+
 
 import { HomeScreen } from './src/screens/home/home-screen';
 import { ProfileScreen } from './src/screens/profile/profile-screen';
@@ -22,11 +25,18 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
+          name="Tabs"
+          component={NavigationTabs}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
           name="Home"
           component={HomeScreen}
           options={{ title: 'Welcome' }}
         />
         <Stack.Screen name="Profile" component={ProfileScreen} />
+
+
       </Stack.Navigator>
     </NavigationContainer>
   );
