177e3fa5e48ac4cad1d46ae4d1967b9c15191414
VictorAponteParedes <vaponte520@gmail.com>
2024-06-24 06:16:09 -0400

    Agrega: comparacion de version de app en las stores

--- App.tsx
+++ App.tsx
@@ -1,21 +1,45 @@
 import * as React from 'react';
+import {useEffect, useState} from 'react';
 import {View, Text} from 'react-native';
-
+import checkVersion from 'react-native-store-version';
 import Toast from 'react-native-toast-message';
 
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
-//NAVIGATIONS
-import {NavigationTabs} from './src/navigation/navigation-tab';
+// NAVIGATIONS
 import {MyNavigationStack} from './src/navigation/navigations-screen';
 
-import {HomeScreen} from './src/screens/home/home-screen';
-import {ProfileScreen} from './src/screens/profile/profile-screen';
-
-const Stack = createNativeStackNavigator();
-
 const App = () => {
+  const [showModalVersion, setShowModalVersion] = useState<boolean>(false);
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const check = await checkVersion({
+          version: '1.0.25', // versión local de la app
+          iosStoreURL:
+            'https://apps.apple.com/py/app/nueva-americana/id6450839651',
+          androidStoreURL:
+            'https://play.google.com/store/apps/details?id=com.konecta.tna',
+          country: 'jp', // valor por defecto es 'jp'
+        });
+
+        if (check.result === 'new') {
+          // si la versión de la app en la tienda es nueva
+          console.log('Hay una nueva versión disponible en la Play Store');
+          setShowModalVersion(true);
+        } else if (check.result === 'equal') {
+          // si la versión de la app en la tienda es igual a la versión local
+          console.log(
+            'La versión de la Play Store es igual a la versión local',
+          );
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    init();
+  }, []);
+
   return (
     <>
       <MyNavigationStack />
