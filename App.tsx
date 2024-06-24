a19c1b7a92c6ccb77b2855a2716111e242159fd3
VictorAponteParedes <vaponte520@gmail.com>
2024-06-23 23:01:29 -0400

    Agrea mensaje Toas  para toda app

--- App.tsx
+++ App.tsx
@@ -1,28 +1,27 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import {View, Text} from 'react-native';
 
+import Toast from 'react-native-toast-message';
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 //NAVIGATIONS
-import { NavigationTabs } from "./src/navigation/navigation-tab";
-import { MyNavigationStack } from "./src/navigation/navigations-screen";
+import {NavigationTabs} from './src/navigation/navigation-tab';
+import {MyNavigationStack} from './src/navigation/navigations-screen';
 
-
-import { HomeScreen } from './src/screens/home/home-screen';
-import { ProfileScreen } from './src/screens/profile/profile-screen';
-
-
+import {HomeScreen} from './src/screens/home/home-screen';
+import {ProfileScreen} from './src/screens/profile/profile-screen';
 
 const Stack = createNativeStackNavigator();
 
-
 const App = () => {
   return (
-    <MyNavigationStack />
+    <>
+      <MyNavigationStack />
+      <Toast />
+    </>
   );
 };
-
 
 export default App;
--- src/components/header-navigation/header-navigation-component.tsx
+++ src/components/header-navigation/header-navigation-component.tsx
@@ -3,6 +3,7 @@ import {View, Text, Image, StyleSheet, TouchableOpacity} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import {IconComponent} from '../icon-custom/icon-component';
 import {FontAwesomeIcon, IoniconsIcon} from '../icon-custom/icon-component';
+import Toast from 'react-native-toast-message';
 
 type headerTypes = {
   title?: string;
@@ -16,21 +17,37 @@ type headerTypes = {
 export const HeaderNavigateComponent = (props: headerTypes) => {
   const {imagenIzq, imagenDer, title, iconName, iconColor, iconSize} = props;
   const navigation = useNavigation();
+
+  const showToast = () => {
+    Toast.show({
+      type: 'error',
+      text1: 'This is an info message',
+      text2: 'Este es un texto numero dos para saber',
+    });
+  };
   return (
-    <View style={styles.container}>
+    <>
+      <View style={styles.container}>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.goBack();
+          }}>
+          {iconName ? (
+            <IoniconsIcon name={iconName} size={iconSize} color={iconColor} />
+          ) : (
+            <Image source={imagenIzq} style={styles.image} />
+          )}
+        </TouchableOpacity>
+        <Text style={styles.title}>{title ? title : 'Header componente'}</Text>
+        <Image source={imagenDer} style={styles.image} />
+      </View>
       <TouchableOpacity
         onPress={() => {
-          navigation.goBack();
+          showToast();
         }}>
-        {iconName ? (
-          <IoniconsIcon name={iconName} size={iconSize} color={iconColor} />
-        ) : (
-          <Image source={imagenIzq} style={styles.image} />
-        )}
+        <Text style={{color: 'red'}}>Show</Text>
       </TouchableOpacity>
-      <Text style={styles.title}>{title ? title : 'Header componente'}</Text>
-      <Image source={imagenDer} style={styles.image} />
-    </View>
+    </>
   );
 };
 
